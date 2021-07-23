b899c702f0d82d37f0db99de1146e47a9e3bb8d8
Natalia <medaigualqueseafacil@gmail.com>
2021-07-23 10:29:19 -0300

    about hecho

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 const port = 3030;
 
-app.use(express.static("public"))
+app.use(express.static('public'));
 
-app.get("/home", (req, res)=> res.sendFile(path.join(__dirname, "views","home.html")))
-app.get("/about", (req, res)=> res.sendFile(path.join(__dirname, "views",)))
-app.get("/music", (req, res)=> res.sendFile(path.join(__dirname, "views",)))
-app.get("/contact", (req, res)=> res.sendFile(path.join(__dirname, "views",)));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views','about.html')));
+app.get('/music', (req, res) => res.sendFile(path.join(__dirname, 'views','music.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views','contact.html')));
 
-app.listen(port, ()=>console.log("servidor en el puerto" + port))
+
+app.listen(port, () => console.log('servidor en el puerto' + port))
